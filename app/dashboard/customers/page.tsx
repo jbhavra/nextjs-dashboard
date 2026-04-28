c90b4523887fb367085683779c741101a6318a2e
jatin <jbhavra@live.com>
2026-04-28 21:25:10 +0530

    added Customer component

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,29 @@
+import { lusitana } from '@/app/ui/fonts';
 import { Metadata } from 'next';
- 
+import CustomersTable from '@/app/ui/customers/table';
+import { Suspense } from 'react';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import Search from '@/app/ui/search';
+
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+    }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+      </div>
+      <Suspense>
+        <CustomersTable customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
